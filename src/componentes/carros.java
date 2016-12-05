e91d0f648afc32a41b824dc643607470e1c5d41c
AlejandroGs13 <Alejandro@Cain>
2016-12-05 14:17:52 -0600

    Codigo elementos en clases- version Delta 0.2

---
+++ src/componentes/carros.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package componentes;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class carros {
+    public int x;
+    public int y;
+    public double vx;
+    public double vy;
+    
+    public carros(int x,int y, double vx,double vy){
+     this.x = x;
+     this.y = y;
+     this.vx = vx;
+     this.vy = vy;
+    }
+}
---
+++ src/componentes/semaforos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package componentes;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class semaforos {
+    
+    public int y;
+    public int ancho;
+    
+    public semaforos(int y,int ancho){
+    this.y = y;
+    this.ancho = ancho;
+    }
+    
+}
